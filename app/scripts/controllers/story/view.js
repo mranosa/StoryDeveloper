f3f513e03fae5836a18a4fa97c674d79f9aeed79
mranosa <mark.ranosa@gmail.com>
2013-01-20 23:44:03 +0800

    Added comments persistence support

--- app/scripts/controllers/story/view.js
+++ app/scripts/controllers/story/view.js
@@ -1,15 +1,18 @@
 'use strict';
 
-StoryDeveloperApp.controller('Story/ViewCtrl', function($scope, $location) {
+StoryDeveloperApp.controller('Story/ViewCtrl', function($scope, $location, UserService) {
 
   var VERSIONS_URL = 'https://story-developer.firebaseIO.com/stories/' + $location.search().storyId + '/versions';
   var versionsRef = new Firebase(VERSIONS_URL);
+  var COMMENTS_URL = 'https://story-developer.firebaseIO.com/stories/' + $location.search().storyId + '/comments';
+  var commentsRef = new Firebase(COMMENTS_URL);
 
   $scope.story = {};
   $scope.title = $location.search().title;
   $scope.storyId = $location.search().storyId;
+  $scope.comments = [];
 
-  $scope.showStory  = function(){
+  $scope.showStory = function(){
       $.pnotify({
           title: 'Getting Latest Version',
           text: 'Please wait for latest version of the story.'
@@ -28,5 +31,31 @@ StoryDeveloperApp.controller('Story/ViewCtrl', function($scope, $location) {
       });
   };
 
+  $scope.addComment = function(){
+      commentsRef.push({
+          author: UserService.name,
+          comment: $('#inputEditorSimple').val()
+      });
+
+      $.pnotify({
+          title: 'New Comment',
+          text: 'You added a new comment.'
+      });
+
+      $('#inputEditorSimple').val('');
+      $scope.listComments();
+  };
+
+  $scope.listComments = function(){
+      commentsRef.once('value', function(snapshot){
+          snapshot.forEach(function(childSnapshot) {
+              $scope.$apply(function(){
+                  $scope.comments.push(childSnapshot.val());
+              });
+          });
+      });
+  };
+
   $scope.showStory();
+  $scope.listComments();
 });
